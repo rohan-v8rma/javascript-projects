3b57ad19d942fdd1c6070adfcf3e28c514556247
Rohan Verma <rv725555@gmail.com>
2022-08-15 12:50:28 +0530

    improve: Added a function for inverting the hex code. Added some technical explanations.

--- color-palette-generator/script.js
+++ color-palette-generator/script.js
@@ -3,7 +3,7 @@ TODO
 
 Add event listener for keyboard to change colors
 Add event listener for button click
-
+Detect if shade is too dark, and change hex code text color to white. Or invert the color
 */
 
 function hexGen() { // This function returns a string like "#287EDF" which can be used to set the backgroundColor of an element.
@@ -19,16 +19,51 @@ function hexGen() { // This function returns a string like "#287EDF" which can b
     return hex;
 }
 
-let hexAssign;
+function hexInvert(hexCode) {
+    let invertedHexCode = "#";
+    let invertDigit;
+  
+    for(let digit = 1; digit <= 6; digit++) {
+        if ( ( 65 <= hexCode.charCodeAt(digit) ) && ( hexCode.charCodeAt(digit) <= 70 ) ) {
+            invertDigit = 15 - ( (hexCode[digit]).charCodeAt(0) - 65 + 10 );
+        }
+        else {
+            invertDigit = 15 - parseInt(hexCode[digit]);
+            
+            if (invertDigit >= 10) {
+              invertDigit = String.fromCharCode(invertDigit + 65 - 10);
+            };          
+        }
+  
+        invertedHexCode += invertDigit;
+  
+    }
+    return invertedHexCode;
+  }
 
-for(let box = 1; box <= 5; box++) {
-    hexAssign = hexGen();
-    
-    // Changing the background color of the color-box
-    document.getElementById("color-box-" + box).style.backgroundColor = hexAssign;
 
-    // Changing the hex code displayed on the color-box
-    document.getElementById("hex-" + box).textContent = hexAssign.slice(1, 7); // Removing the hash using slicing
+function palletteChange() {
+    let hexAssign, hexInvertAssign;
+
+    for(let box = 1; box <= 5; box++) {
+        hexAssign = hexGen();
+        hexInvertAssign = hexInvert(hexAssign);
+        
+        // Changing the background color of the color-box
+        document.getElementById("color-box-" + box).style.backgroundColor = hexAssign;
+
+        // Changing the hex code displayed on the color-box
+        document.getElementById("hex-" + box).textContent = hexAssign.slice(1, 7); // Removing the hash using slicing
+
+        /*
+        ! NOTE that if we tried to change the textContent of id 'color-box-X' directly, the whole 'color-hex-code' div Node would be removed and a simple hex-code would be inserted in its place. 
+        
+        This would remove its chosen font, the margin/padding applied to the internal div, etc.
+
+        ? This is because if we set the textContent property of a particular element, all child text nodes are replaced by only one new text node.
+        */
+    };
 }
 
+palletteChange();
 
